840a705f8d1ea32dce50fde091980f343fef655c
0ManasVerma0 <manasverma1357@gmail.com>
2026-02-24 23:55:39 +0530

    nodemon & env setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import dotenv from "dotenv"
+dotenv.config({
+    path: "./.env",
+})
+
+let myUsername = process.env.test;  
+console.log(myUsername);
+
 console.log("start of backend");
+console.log("testing nodemon");
+
