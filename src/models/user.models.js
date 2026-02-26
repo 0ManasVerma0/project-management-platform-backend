6ddd2dce3b1ac149e65c57336843e9d239b6068c
0ManasVerma0 <manasverma1357@gmail.com>
2026-02-26 19:46:36 +0530

    token generation

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,7 @@
-import mongoose, {mongo, Schema} from "mongoose";
+import mongoose, { mongo, Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const UserSchema = new Schema({
 
@@ -16,7 +18,7 @@ const UserSchema = new Schema({
 
     username: {
         type: String,
-        required: true, 
+        required: true,
         unique: true,
         lowercase: true,
         trim: true,
@@ -61,28 +63,64 @@ const UserSchema = new Schema({
     emailVerificationToken: {
         type: String
     },
-    
+
     emailVerificationExpiry: {
         type: Date
     }
 },
-{
-    timestamps: true
-})
+    {
+        timestamps: true
+    })
 
 //defining a pre hook
 //pre hook => this runs just before saving the schema data in database
-UserSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next()  //this will save from the cases where password is not changed
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()  //this will save from the cases where password is not changed
 
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
 //In this way you can define methods in mongoose
-UserSchema.methods.isPassword = async function(password){
+UserSchema.methods.isPassword = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
+
+//Generation of JSON web token access and refresh token 
+
+//Data tokens
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this.id
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+}
+
+//without data tokens
+UserSchema.methods.generateTemporaryToken = function () {
+    const unhashedToken = crypto.randomBytes(20).toString("hex")
+
+    const hashedToken = crypto.createHash("sha256")
+        .update(unhashedToken)
+        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20 * 60 * 1000) //20 mins
+    return {unhashedToken, hashedToken, tokenExpiry}
+}
+
 
 export const User = mongoose.model("User", UserSchema)
 
