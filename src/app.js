fbaf85478a998dfe0cce8d845b50ba53ae6d2c2b
0ManasVerma0 <manasverma1357@gmail.com>
2026-02-26 13:33:02 +0530

    standardize api response & error

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"))
 
 //cors config
 app.use(cors({
-    origin: process.end.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTION"],
     allowedHeaders: ["Content-Type", "Authorization"]
