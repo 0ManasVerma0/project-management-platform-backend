1f420339d284e40c9d829bf52df0753340d89cd2
0ManasVerma0 <manasverma1357@gmail.com>
2026-02-26 13:42:20 +0530

    add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const userRoleEnums = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member"  
+}
+
+export const AvailableUserRole = Object.values(userRoleEnums)
+
+export const TaskStatusEnums = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"
+}
+
+export const AvailableTaskStatuses = Object.values(TaskStatusEnums)
