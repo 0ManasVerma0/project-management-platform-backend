77c18134e08c4253f9fb641f4a217af518f6d357
0ManasVerma0 <manasverma1357@gmail.com>
2026-04-08 16:59:51 +0530

    add role base permission middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,7 +1,9 @@
 import { User } from "../models/user.models.js";
+import {ProjectMember} from "../models/projectmember.models.js"
 import { apiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import jwt, { decode } from "jsonwebtoken";
+import mongoose from "mongoose";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
@@ -25,3 +27,31 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         throw new apiError(401, "Invalid Access Token")
     }
 })
+
+export const validateProjectPermission = (roles = []) => {
+    asyncHandler(async (req, res, next) => {
+        const {projectId} = req.params
+        if(!projectId){
+            throw new apiError(400, "Project ID is missing")
+        }
+
+        const project = await ProjectMember.findOne({
+            project: new mongoose.Types.ObjectId(projectId),
+            user: new mongoose.Types.ObjectId(req.user._id)
+        })
+
+        if(!project){
+            throw new apiError(400, "Project Not Found")
+        }
+
+        const givenRole = project?.role
+        req.user.role = givenRole
+
+        if(!roles.includes(givenRole)){
+            throw new apiError(403, "You Don't have permission to Perform this Action")
+        }
+
+        next()
+
+    })
+}
